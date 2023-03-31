3da3504c623aa62ae0cf2f2f474a7739b03744c9
Tarun Kumar <109733908+TarunKumar19@users.noreply.github.com>
2023-03-31 14:49:31 +0000

    Added connection to DB

--- app.py
+++ app.py
@@ -1,21 +1,41 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
-
+from database import engine, load_jobs_from_db
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
+
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'location': 'Benguluru , India',
+  'Salary': 'Rs. 1,000,00'
+}, {
+  'id': 2,
+  'title': 'Data Scientist',
+  'location': 'Benguluru , India',
+  'Salary': 'Rs. 5,600,00'
+}, {
+  'id': 3,
+  'title': 'FrontEnd Engineer',
+  'location': 'Patna , India',
+  'Salary': 'Rs. 2,000,00'
+}, {
+  'id': 4,
+  'title': 'BackEnd Engineer',
+  'location': 'Kolkata , India',
+  'Salary': '$ 4,00,00'
+}]
+
 
 @app.route("/")
 def hello():
   jobs = load_jobs_from_db()
-  return render_template('home.html',
-                         jobs=jobs,   
-                         company_name='Automation')
+  return render_template('home.html', jobs=jobs, company_name='Automation')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  jobs = load_jobs_from_db()
-  return jsonify(jobs)
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,14 +1,12 @@
-from sqlalchemy import create_engine,text
+from sqlalchemy import create_engine, text
+import os
 
-db_connection_string= "mysql+pymysql://l9dyp41ijpctbpybkq4w:pscale_pw_G3bX2msbFNIQeAcj73XiJYmaD0ierOlG8fs2OOgpq5y@aws.connect.psdb.cloud/automationcarrer?charset=utf8mb4"
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
-engine = create_engine(
-  db_connection_string,
-  connect_args={
-  "ssl": {
-  "ssl_ca": "/etc/ssl/cert.pem"         
-      }
-  })
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem"
+                       }})
 
 
 def load_jobs_from_db():
